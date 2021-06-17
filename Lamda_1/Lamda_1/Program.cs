bd9fa301212933a6e6bf6a28b67468c49a9ede68
DESKTOP-30RJMTC\kimo8 <kimo87@gmail.com>
2021-06-17 23:59:51 +0900

    1일 1 커밋

---
+++ Lamda_1/Lamda_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamda_1
+{
+    class Program
+    {
+        delegate void Lambda();
+        static void Main(string[] args)
+        {
+            Lambda hi =
+                () => Console.WriteLine("안녕하세요");
+            hi();
+
+        }
+    }
+}
